43391c3acd3c3296c20239b95cabf5b56912bcb3
Engin Can <enginyakar4242@gmail.com>
2025-04-27 15:57:53 +0300

    Add files via upload

---
+++ odev1.kt
@@ -0,0 +1,53 @@
+package com.example.kotlin.odevlerim
+
+class odev1 {
+    var sehir = "İstanbul"
+    var ulke = "Türkiye"
+    var telefon = "05551234567"
+    var postaKodu = "34000"
+    var email = "ornek@mail.com"
+    var meslek = "Yazılım Mühendisi"
+    var stokMiktari = 150
+    var musteriAdi = "Ahmet Yılmaz"
+    var bakiye = 3200.75
+    var dogumGunu = "1995-05-23"
+    var maas = 25000
+    var medeniDurum = "Bekar"
+    var urunYorum = "Harika ürün!"
+
+    var odemeTarihi = "2025-05-01"
+    var odeme = 1200
+    var siparisAdeti = 3
+    var arabaModeli = "Toyota Corolla"
+    var kitapAdi = "Kotlin Öğreniyorum"
+    var yayinlamaTarihi = "2023-09-15"
+    var indirimMiktari = 20
+    var odaSayisi = 2
+    var enlem = 41.0082
+    var boylam = 28.9784
+    var urunAdi = "Laptop"
+    var yemekFiyati = 75.50
+
+    var marka = "Apple"
+    var muzikAdi = "Imagine"
+    var videoSuresi = "3:45"
+    var urunPuani = 4.8
+    var resimAdi = "urun_resmi.png"
+    var dosyaFormati = "jpg"
+    var renk = "Mavi"
+    var renkKodu = "#0000FF"
+    var telefonModeli = "iPhone 14"
+    var ekranBoyutu = 6.1
+    var agirlik = 0.174
+
+    var ulusalGun = "29 Ekim"
+    var tatilGunu = "1 Mayıs"
+    var rezervasyonTarihi = "2025-07-20"
+    var sokakAdi = "Bağdat Caddesi"
+    var otobusHatti = "500T"
+    var kalanDakika = 15
+    var takipKodu = "TR123456789"
+    var kuponSuresi = 30 // gün
+    var kuponKodu = "INDIRIM50"
+    var faturaAdresi = "Kadıköy, İstanbul"
+}
